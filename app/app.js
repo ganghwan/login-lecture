649980268e6954b67a863c922872c08f27103442
ganghwan <ghp@softplant.co.kr>
2022-08-12 13:35:56 +0900

    프론트 처리

--- app/app.js
+++ app/app.js
@@ -3,17 +3,22 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require( "body-parser");
 const app = express();
 
 
 
 // 라우팅
-const home = require("./src/routes/home");
+const home = require("./src/routes/home"); 
 
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+
+//URL을 통해 전달되는 데이터 인식문제 해결
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드.
