aaf42551787996e651b9312af4934673d324116c
ganghwan <ghp@softplant.co.kr>
2022-08-19 08:38:33 +0900

    회원가입요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,36 +1,40 @@
 "use strict";
  
 const id = document.querySelector("#id"),
+name = document.querySelector("#name"),
 psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("#button");
+confirmPsword = document.querySelector("#confirm-psword"),
+registerBtn = document.querySelector("#button");
+ 
+registerBtn.addEventListener("click",register);
 
-loginBtn.addEventListener("click",login);
 
-
-function login()
+function register()
 {
     const req = {
         id: id.value,
+        name: name.value,
         psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
- 
+  
 
-    fetch("/login",{
+    fetch("/register",{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(req),
     })
-    .then((res)=> res.json())
+    .then((res)=> res.json())  
     .then((res)=> {
         if (res.success){
-            location.href="/";
+            location.href="/login";
         } else {
             alert(res.msg);
         }
     })
     .catch((err)=> {
-        console.error("로그인중 에러발생");
+        console.error("회원가입중 에러발생");
     })
 }
